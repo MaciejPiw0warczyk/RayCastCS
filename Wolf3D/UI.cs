32efb2a515dc9ac9d9c7ad4cf77bdf5689af8844
Maciej <mpiwo4@gmail.com>
2022-09-30 13:45:54 +0200

    added ui class

--- Wolf3D/UI.cs
+++ Wolf3D/UI.cs
@@ -14,15 +14,14 @@ namespace MapCreator.Wolf3D
         {
             float[] UIvert =
             {
-                    530, DisplayManager.WindowSize.Y,                           .6f,.4f,.1f,
-                    530, DisplayManager.WindowSize.Y-50,                                                    .6f,.4f,.1f,
-                    DisplayManager.WindowSize.X, DisplayManager.WindowSize.Y-50,.6f,.4f,.1f,
+                    530, DisplayManager.WindowSize.Y,                            .6f,.4f,.1f,
+                    530, DisplayManager.WindowSize.Y-50,                         .6f,.4f,.1f,
+                    DisplayManager.WindowSize.X, DisplayManager.WindowSize.Y-50, .6f,.4f,.1f,
 
-                    DisplayManager.WindowSize.X, DisplayManager.WindowSize.Y-50,.6f,.4f,.1f,
-                    DisplayManager.WindowSize.X, DisplayManager.WindowSize.Y,   .6f,.4f,.1f,
-                    530, DisplayManager.WindowSize.Y,                           .6f,.4f,.1f,
+                    DisplayManager.WindowSize.X, DisplayManager.WindowSize.Y-50, .6f,.4f,.1f,
+                    DisplayManager.WindowSize.X, DisplayManager.WindowSize.Y,    .6f,.4f,.1f,
+                    530, DisplayManager.WindowSize.Y,                            .6f,.4f,.1f,
             };
-
             return UIvert;
         }
     }
