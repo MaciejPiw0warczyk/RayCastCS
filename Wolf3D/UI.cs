c7bae001312380917cced59ff16f895c5b098505
Maciej <mpiwo4@gmail.com>
2022-09-30 13:11:03 +0200

    added ui class

---
+++ Wolf3D/UI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapCreator.Rendering.Display;
+
+namespace MapCreator.Wolf3D
+{
+    static class UI
+    {
+        //TODO add texture as a test
+        public static float[] DrawUI()
+        {
+            float[] UIvert =
+            {
+                    530, DisplayManager.WindowSize.Y,                           .6f,.4f,.1f,
+                    530, DisplayManager.WindowSize.Y-50,                                                    .6f,.4f,.1f,
+                    DisplayManager.WindowSize.X, DisplayManager.WindowSize.Y-50,.6f,.4f,.1f,
+
+                    DisplayManager.WindowSize.X, DisplayManager.WindowSize.Y-50,.6f,.4f,.1f,
+                    DisplayManager.WindowSize.X, DisplayManager.WindowSize.Y,   .6f,.4f,.1f,
+                    530, DisplayManager.WindowSize.Y,                           .6f,.4f,.1f,
+            };
+
+            return UIvert;
+        }
+    }
+}
